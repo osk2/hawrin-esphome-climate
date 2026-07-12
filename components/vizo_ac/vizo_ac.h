0392e6a2c56229003544d92a256b383f27f1b140
osk2 <sc60714@gmail.com>
2026-07-12 17:15:36 +0800

    Add vizo_ac ESPHome component

---
+++ components/vizo_ac/vizo_ac.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "esphome/components/climate_ir/climate_ir.h"
+
+namespace esphome::vizo_ac {
+
+class VizoACClimate : public climate_ir::ClimateIR {
+ public:
+  VizoACClimate()
+      : climate_ir::ClimateIR(
+            20, 30, 1.0f, false, false,
+            {climate::CLIMATE_FAN_AUTO, climate::CLIMATE_FAN_LOW, climate::CLIMATE_FAN_MEDIUM,
+             climate::CLIMATE_FAN_HIGH},
+            {}, {climate::CLIMATE_PRESET_NONE, climate::CLIMATE_PRESET_ECO}) {}
+
+  void control(const climate::ClimateCall &call) override;
+  void send_display_toggle();
+
+ protected:
+  void transmit_state() override;
+  void send_power_toggle_();
+  void send_eco_(bool enabled);
+  void send_frame_(uint8_t byte2, uint8_t byte3, uint8_t byte6, uint8_t byte7, const uint8_t *frame2);
+  static uint8_t temp_byte_(float temp);
+  static uint8_t fan_byte_(optional<climate::ClimateFanMode> fan);
+  static uint8_t seq_for_temp_(float temp);
+};
+
+}  // namespace esphome::vizo_ac
